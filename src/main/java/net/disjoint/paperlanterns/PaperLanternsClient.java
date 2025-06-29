9c23de0d28b336efcc257a687114e75ea10a65cb
disjoint-account <108885466+disjoint-account@users.noreply.github.com>
2025-06-29 22:46:15 +0100

    Updated to Minecraft 1.21.6

--- src/main/java/net/disjoint/paperlanterns/PaperLanternsClient.java
+++ src/main/java/net/disjoint/paperlanterns/PaperLanternsClient.java
@@ -1,13 +1,13 @@
 package net.disjoint.paperlanterns;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.render.RenderLayer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class PaperLanternsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(PaperLanternsBlocks.PAPER_LANTERN, RenderLayer.getCutout());
+        BlockRenderLayerMap.putBlock(PaperLanternsBlocks.PAPER_LANTERN, BlockRenderLayer.CUTOUT);
     }
 }
--- src/main/java/net/disjoint/paperlanterns/datagen/PaperLanternsBlockTagProvider.java
+++ src/main/java/net/disjoint/paperlanterns/datagen/PaperLanternsBlockTagProvider.java
@@ -4,9 +4,10 @@ import net.disjoint.paperlanterns.PaperLanternsBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.BlockTags;
 
 import java.util.concurrent.CompletableFuture;
+
+import static net.minecraft.registry.tag.BlockTags.PICKAXE_MINEABLE;
 
 public class PaperLanternsBlockTagProvider extends FabricTagProvider.BlockTagProvider {
     public PaperLanternsBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
@@ -15,7 +16,7 @@ public class PaperLanternsBlockTagProvider extends FabricTagProvider.BlockTagPro
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+        valueLookupBuilder(PICKAXE_MINEABLE)
                 .add(PaperLanternsBlocks.PAPER_LANTERN);
     }
 }
